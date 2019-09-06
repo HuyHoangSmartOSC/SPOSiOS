46d37db1dc3290566f97fdff29abf5fb63adb566
HuyHoangSmartOSC <53812377+HuyHoangSmartOSC@users.noreply.github.com>
2019-09-06 18:19:05 +0700

    Add files via upload

---
+++ DemoProjectUsedAPI/DataManagement.swift
@@ -0,0 +1,13 @@
+//
+//  DataManagement.swift
+//  DemoProjectUsedAPI
+//
+//  Created by smart on 9/4/19.
+//  Copyright © 2019 smart. All rights reserved.
+//
+
+import Foundation
+
+class DataController {
+    func
+}
---
+++ DemoProjectUsedAPI/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DemoProjectUsedAPI
+//
+//  Created by smart on 9/4/19.
+//  Copyright © 2019 smart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - Outlet
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
